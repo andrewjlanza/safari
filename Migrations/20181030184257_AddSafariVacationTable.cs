dd67f3c32cffddb8e344ba8104b437b705d05615
Andrew Lanza <andrewlanza@Andrews-MBP-2.localdomain>
2018-10-30 15:22:30 -0400

    added ef

---
+++ Migrations/20181030184257_AddSafariVacationTable.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace safari.Migrations
+{
+    public partial class AddSafariVacationTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SafariVacation",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Species = table.Column<string>(nullable: true),
+                    CountOfTimesSeen = table.Column<int>(nullable: false),
+                    LocationOfLastSeen = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SafariVacation", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SafariVacation");
+        }
+    }
+}
